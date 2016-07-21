f31dbda1f0d496329c75b75323fef806b8d18629
andrew morton <drewish@katherinehouse.com>
2016-07-21 10:18:47 -0600

    WIP download linked file

--- lib/recurly/export_file.php
+++ lib/recurly/export_file.php
@@ -9,5 +9,12 @@ class Recurly_ExportFile extends Recurly_Resource
   protected function getWriteableAttributes() {
     return array();
   }
+
+  function download($file_pointer) {
+    if (empty($this->download_url)) {
+      $this->_save(Recurly_Client::GET, $this->getHref());
+    }
+    $this->_client->getFile($this->download_url, $file_pointer);
+  }
 }
 
